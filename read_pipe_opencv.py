35b33566340b5beef6916c135bfe7dd2603b6049
Begaev Alexey <ph.begaev@gmail.com>
2018-11-09 12:58:37 +0300

    work script

--- read_pipe_opencv.py
+++ read_pipe_opencv.py
@@ -10,19 +10,20 @@ command = [ FFMPEG_BIN,
         '-an','-sn',              # we want to disable audio processing (there is no audio)
         '-f', 'image2pipe', '-']
 pipe = sp.Popen(command, stdout = sp.PIPE, bufsize=10**8)
-
+cv2.namedWindow('Video', cv2.WINDOW_GUI_NORMAL)
 while True:
     # Capture frame-by-frame
-    raw_image = pipe.stdout.read(2688*1520*3)
+    raw_image = pipe.stdout.read(1520*2688*3)
     # transform the byte read into a numpy array
     image =  numpy.fromstring(raw_image, dtype='uint8')
+    # print(image.shape)
+    image = image.reshape((1520,2688,3))          # Notice how height is specified first and then width
     print(image.shape)
-    # image = image.reshape((480,640,3))          # Notice how height is specified first and then width
-    # if image is not None:
-    #     cv2.imshow('Video', image)
-    #
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     break
+    if image is not None:
+        cv2.imshow('Video', image)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
     pipe.stdout.flush()
 
 cv2.destroyAllWindows()
